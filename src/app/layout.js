08b948367052754ed7d4356472fd7dd4fc8c9147
Ilonchik11 <ilonka7011@gmail.com>
2026-09-08 15:30:45 +0300

    api intergration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { AppContextProvider } from "@/context/AppContext";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 const inter = Inter({
@@ -18,7 +19,15 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <AppContextProvider>
         <html lang="en" className={`${inter.className} antialiased`}>
-          <body className="min-h-full flex flex-col">{children}</body>
+          <body className="min-h-full flex flex-col">
+            <Toaster toastOptions={
+              {
+                success: { style: {background: "black", color: "white"}},
+                error: { style: {background: "black", color: "white"}},
+              }
+            } />
+            {children}
+          </body>
         </html>
       </AppContextProvider>
     </ClerkProvider>
